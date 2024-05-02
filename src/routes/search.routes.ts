84d05dcb3d13504daf3ecceec5c53d1183159475
fabianoreisf <fabiano.reis@apinteractive.com.br>
2024-05-02 14:55:05 -0300

    TASK-8: input validation with joi

--- src/routes/search.routes.ts
+++ src/routes/search.routes.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express'
 import RoomController from '../controllers/RoomController'
+import { celebrate, Segments, Joi } from 'celebrate'
 
 const searchRouter = Router()
 const searchController = new RoomController()
 
-searchRouter.post('/', searchController.index)
+searchRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      checkin: Joi.string().required(),
+      checkout: Joi.string().required(),
+    },
+  }),
+  searchController.index,
+)
 
 export default searchRouter
